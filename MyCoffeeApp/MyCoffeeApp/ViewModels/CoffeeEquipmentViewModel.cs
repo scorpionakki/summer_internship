3866700d319d00eca37501e98c1e9bf169c54b22
Alkesh Kataria <202012056@daiict.ac.in>
2021-06-17 20:40:26 +0530

    Explored about MVVM architecture and played around with the counter using a button. A counter will increase the count by one on button click.

---
+++ MyCoffeeApp/MyCoffeeApp/ViewModels/CoffeeEquipmentViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MyCoffeeApp.ViewModels
+{
+    public class CoffeeEquipmentViewModel : BindableObject
+    {
+        public CoffeeEquipmentViewModel()
+        {
+            IncreaseCount = new Command(OnIncrease);
+        }
+
+        public ICommand IncreaseCount { get; }
+
+        private int count = 0;
+        private string countDisplay = "0";
+
+        public string CountDisplay
+        {
+            get => countDisplay;
+            set
+            {
+                if (value == countDisplay)
+                    return;
+                countDisplay = value;
+                OnPropertyChanged();
+            }
+        }
+
+        void OnIncrease()
+        {
+            count++;
+            CountDisplay = count.ToString();
+        }
+    }
+}
